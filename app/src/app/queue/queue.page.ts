5da6a387c307d77ff3257edcbe12e5d55c76bb11
Cuddly Cheetah <github.konrad@sch.umann.it>
2019-10-09 02:37:29 +0200

    added APIs

--- app/src/app/queue/queue.page.ts
+++ app/src/app/queue/queue.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { APIQueueService } from '../api/apiqueue.service';
 
 @Component({
   selector: 'app-queue',
   templateUrl: './queue.page.html',
   styleUrls: ['./queue.page.scss'],
 })
-export class QueuePage implements OnInit {
+export class QueuePage implements OnInit, OnDestroy {
+  private session;
+  public socketData = {
+    connectionState: 'Unbekannt',
+    queueData: {
+      queue: [],
+      waiting: [],
+    },
+  };
 
-  constructor() { }
+  constructor(
+    private apiQueue: APIQueueService,
+  ) {
+  }
 
   ngOnInit() {
+    this.session = this.apiQueue.getSocket()
+    .subscribe((data: any) => this.socketData = data);
+  }
+  ngOnDestroy() {
+    if (!!this.session) { this.session.unsubscribe(); }
   }
 
 }
