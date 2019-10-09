5794cf1fa1bd90fa3eb89dd21bee0e6f0a70ec3d
Cuddly Cheetah <github.konrad@sch.umann.it>
2019-10-09 02:38:49 +0200

    Revert "Revert "added APIs""
    This reverts commit f43a2976b18bfa3abee394cc195fa40a9da65207.

--- app/src/app/berichtsheft/berichtsheft.page.ts
+++ app/src/app/berichtsheft/berichtsheft.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { APIBerichtshefteService } from '../api/apiberichtshefte.service';
+import { Berichtsheft } from '../api/berichtsheft';
 
 @Component({
   selector: 'app-berichtsheft',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./berichtsheft.page.scss'],
 })
 export class BerichtsheftPage implements OnInit {
+  private displayedColumns = ['name', 'kwoffset', 'uuid'];
+  public listBerichtshefte: Berichtsheft[] = [];
 
-  constructor() { }
-
+  constructor(
+    private apiBerichtsheft: APIBerichtshefteService,
+  ) { }
   ngOnInit() {
+    this.refresh();
+  }
+  selectBerichtsheft(row) {
+    console.log(row);
+  }
+  async refresh() {
+    (await this.apiBerichtsheft.getAll())
+    .subscribe((data: Berichtsheft[]) => {
+      this.listBerichtshefte = data;
+    });
   }
 
 }
--- app/src/app/queue/queue.module.ts
+++ app/src/app/queue/queue.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { QueuePage } from './queue.page';
+import { MaterialModule } from '../material.service';
 
 const routes: Routes = [
   {
@@ -19,6 +20,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    MaterialModule,
     RouterModule.forChild(routes)
   ],
   declarations: [QueuePage]
