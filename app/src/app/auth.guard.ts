ca72db9c45deb253b87390f459c24624aeed9007
Cuddly Cheetah <github.konrad@sch.umann.it>
2019-10-13 03:29:06 +0200

    now you can load urls directly

--- app/src/app/auth.guard.ts
+++ app/src/app/auth.guard.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { WebStorageService, LOCAL_STORAGE } from 'angular-webstorage-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    @Inject(LOCAL_STORAGE) private storage: WebStorageService
+  ) {
+
+  }
 
   canActivate(): boolean {
-    return this.authService.isAuthenticated();
+    return !!this.storage.get('ACCESS_TOKEN');
   }
 }
