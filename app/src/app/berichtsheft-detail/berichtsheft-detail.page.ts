7d3a8232eb6d5803b5bbcb8a1c03e388af6a6986
Cuddly Cheetah <github.konrad@sch.umann.it>
2019-10-27 17:55:45 +0100

    added validators and save detector

--- app/src/app/berichtsheft-detail/berichtsheft-detail.page.ts
+++ app/src/app/berichtsheft-detail/berichtsheft-detail.page.ts
@@ -8,17 +8,7 @@ import { Wochenbericht } from '../api/wochenbericht';
 import { APITemplatesService } from '../api/apitemplates.service';
 import Vorlage from '../api/vorlage';
 import { forkJoin } from 'rxjs';
-
-function freiReasonValidator(c: AbstractControl): any {
-  if (!c.parent || !c) { return;}
-  const krank = c.parent.get('krank');
-  const frei = c.parent.get('frei');
-
-  if (!krank) { return; }
-  if (krank.value === false && frei.value.length < 3) {
-      return { invalid: true };
-  }
-}
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-berichtsheft-detail',
@@ -36,13 +26,14 @@ export class BerichtsheftDetailPage implements OnInit {
   public kwSel = new FormControl('');
   public dayNames = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag'];
 
-  public selectedTemplate = undefined;
+  public selectedTemplate = '';
 
   constructor(
     private route: ActivatedRoute,
     private apiWochenbericht: APIWochenberichteService,
     private apiTemplates: APITemplatesService,
     private formBuilder: FormBuilder,
+    private alertCtrl: AlertController,
   ) {
     this.wochenberichtsForm = this.formBuilder.group({
       id: [''],
@@ -91,6 +82,7 @@ export class BerichtsheftDetailPage implements OnInit {
     .subscribe(res => {
       console.log(res);
       this.wochenberichtDetails = res;
+
       // Formular aus Arrays/Groups rekonstruieren
       if (!!this.wochenberichtDetails.free) {
         this.wochenberichtsForm = this.formBuilder.group({
@@ -102,7 +94,7 @@ export class BerichtsheftDetailPage implements OnInit {
           id: [this.wochenberichtDetails.id, Validators.required],
           tage: this.formBuilder.array(
             this.wochenberichtDetails.tage.map((tag) => {
-              return this.formBuilder.group({
+              const group = this.formBuilder.group({
                 uuid: [tag.uuid, Validators.required],
                 id: [tag.id, Validators.required],
                 start: [tag.start, Validators.required],
@@ -117,12 +109,67 @@ export class BerichtsheftDetailPage implements OnInit {
                   ]))
                 ),
                 krank: [tag.krank],
-                frei: [tag.frei, freiReasonValidator]
+                frei: [tag.frei, tag.krank ? Validators.required : []]
               });
+              group.get('krank').valueChanges.subscribe(
+                (isKrank: boolean) => {
+                  group.get('frei').setValidators(
+                    isKrank
+                      ? [Validators.required]
+                      : []
+                  );
+                  group.get('frei').updateValueAndValidity();
+                }
+              );
+              return group;
             })
           ),
         }); // ende formBuilder <-- ja manchmal sind solche kommentare nützlich um den überblick nicht zu verlieren
       }
     });
+  }
+  async createWochenbericht() {
+    const jahr = this.kwSel.value.split('-W')[0], kw = this.kwSel.value.split('-W')[1];
+    (await this.apiWochenbericht.create(this.berichtsheftDetails.uuid, jahr, kw, this.selectedTemplate))
+    .subscribe(res => {
+      console.log(res);
+      this.loadWochenbericht(jahr, kw);
+    });
+  }
+  async deleteWochenbericht() {
+    const alert = await this.alertCtrl.create({
+      header: 'Bestätigung erforderlich!',
+      message: 'Möchten sie wirklich die Vorlage löschen?',
+      buttons: [
+        {
+          text: 'Abbrechen',
+          role: 'cancel',
+          cssClass: 'secondary',
+        }, {
+          text: 'Ja, löschen!',
+          handler: async () => {
+            const jahr = this.kwSel.value.split('-W')[0], kw = this.kwSel.value.split('-W')[1];
+            (await this.apiWochenbericht.delete(this.berichtsheftDetails.uuid, this.wochenberichtDetails.uuid))
+            .subscribe(res => {
+              console.log(res);
+              this.loadWochenbericht(jahr, kw);
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  async saveChangedTagesberichtsAndWochenbericht() {
+    const original = JSON.stringify({
+      id: this.wochenberichtDetails.id,
+      tage: this.wochenberichtDetails.tage.map(tag => {
+        delete tag.erweitert; // remove legacy shitfuck
+        return tag;
+      }),
+    });
+    const formVersion = JSON.stringify(this.wochenberichtsForm.value);
+    console.log('changed', original !== formVersion, original, formVersion);
   }
 }
